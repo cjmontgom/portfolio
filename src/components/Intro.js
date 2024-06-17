1e58354923253c3b8fc9ed15396da7180ab037cb
chloe <chloemontgomery01@gmail.com>
2024-06-17 12:25:05 +0100

    Finalise Intro copy and font

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,43 +2,47 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  margin: 30px;
+  margin: auto;
+  padding: 80px 0px 100px;
+  max-width: 900px;
   text-align: center;
   display: flex;
   flex-direction: column;
 `;
 
-const Paragraph = styled.p`
-  font-size: 3rem;
-  max-width: 1000px;
-  font-family: Plus Jakarta Sans;
+const ZanyIntroFont = styled.span`
+  font-size: 4rem;
+  font-family: Cormorant Garamond, serif;
+  font-weight: 400;
+  font-style: italic;
 `;
 
-const Span = styled.span``;
-
-const Heading = styled.h2`
-  font-size: 2rem;
-  margin: 0.5rem 0;
+const IntroFont = styled.span`
+  font-size: 3rem;
+  font-weight: 300;
+  font-family: Plus Jakarta Sans;
 `;
 
 const HorizontalRule = styled.hr`
   height: 1px;
-  width: 100%;
+  width: 90%;
   border: 0;
   border-top: 1px solid ${(props) => props.theme.text};
 `;
 
 const Intro = () => {
   return (
-    <Container>
-      <Paragraph>
-        I'm a football player, woodworker, baseball enthusiast, and
-        <br />
-        <Span> Software Developer </Span> extraordinaire
-      </Paragraph>
-
+    <>
+      <Container>
+        <IntroFont>
+          Hello, I'm Chloe... football<ZanyIntroFont> player</ZanyIntroFont>,
+          woodworker, baseball
+          <ZanyIntroFont> enthusiast</ZanyIntroFont>, and <br />
+          <ZanyIntroFont> Software Developer </ZanyIntroFont>extraordinaire
+        </IntroFont>
+      </Container>
       <HorizontalRule />
-    </Container>
+    </>
   );
 };
 
